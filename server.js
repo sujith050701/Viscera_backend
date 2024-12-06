8891d100261c0253559e4955f425bd8227b866af
sujith050701 <rsujith776@gmail.com>
2024-12-07 00:10:31 +0530

    mongodb

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.listen(port, () => {
     console.log(`Server is running on Port ${port}`)
 })
 
-mongoose.connect('mongodb+srv://rsujith776:Sujith2005@cluster0.j97lo.mongodb.net/stu?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://rsujith776:Sujith2005@cluster0.j97lo.mongodb.net/clinic?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => console.log('MongoDB connected successfully'))
 .catch((error) => console.error('MongoDB connection error:', error));
